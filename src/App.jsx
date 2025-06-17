08419dd3d3def6f52c2029d77f7adb822df373c7
Daisy <m4rklucky@gmail.com>
2025-06-17 11:13:03 +0700

    change_position

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Info from './assets/info'
+import Info from './info'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Info from './assets/info.jsx'
+import Info from './info.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
